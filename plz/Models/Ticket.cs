493d408fd7293609d874eed68e7c323ba1013a0e
Sali <saliAlmurshidi@gmail.com>
2020-04-15 16:18:42 +0200

    try to add DB

---
+++ plz/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace plz.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName ="nvarchar(250)")]
+        public string Title { get; set; }
+
+    }
+}
---
+++ plz/Models/TicketContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using plz.Models;
+
+namespace plz.Controllers
+{
+    public class TicketContext:DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options):base (options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
