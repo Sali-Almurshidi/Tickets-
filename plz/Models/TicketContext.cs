ddf6edfa91ab81ba54acfb07ddc2ed9904c09095
Sali <saliAlmurshidi@gmail.com>
2020-04-15 16:22:23 +0200

    Test push to githup

--- plz/Models/TicketContext.cs
+++ plz/Models/TicketContext.cs
@@ -11,7 +11,6 @@ namespace plz.Controllers
     {
         public TicketContext(DbContextOptions<TicketContext> options):base (options)
         {
-
         }
 
         public DbSet<Ticket> Tickets { get; set; }
